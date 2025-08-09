f359532863cdcc250aacbe83ce494703923efe02
KimGyunYeop <gyop0817@naver.com>
2025-08-10 03:59:10 +0900

    fix

--- reproduce_test.sh
+++ reproduce_test.sh
@@ -1,13 +1,15 @@
 # !/bin/bash
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
 wandb offline
 adapter_path="GyunYeop/midm-base-GRPO-tuning-KoreanCultureQA"
 
+dev_data_path="data/QA/korean_culture_qa_V1.0_dev+.json" # change to your dev data path
 test_data_path="data/QA/korean_culture_qa_V1.0_test+.json" # change to your test data path
 
 python -u reproduce_test.py \
     --adapter_path "$adapter_path" \
+    --dev_data_path "$dev_data_path" \
     --test_data_path "$test_data_path" \
     --device "cuda:0"
